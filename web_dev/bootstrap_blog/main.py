fb9c39d0dafdeeef2011ad0ff7f82cf7b760671c
Andrej Cepygin <andrej.cepygin@uni-oldenburg.de>
2025-03-14 15:26:01 +0100

    messing around with bootstrap for blog website

---
+++ web_dev/bootstrap_blog/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+all_posts = requests.get(blog_url).json()
+
+@app.route('/')
+def home():
+    return render_template('index.html', posts=all_posts)
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in all_posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
